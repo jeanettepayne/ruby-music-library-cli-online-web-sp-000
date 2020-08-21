f66646eb0416a652afd6442b92cea9d600e02289
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-21 20:43:06 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -31,7 +31,8 @@ class MusicLibraryController
   end
   
   def list_artists
-    
+    Artist.all.sort_by(&:name).each.with_index(1) do |artist, indx|
+      puts "#{indx}. #{artist}"
   end
   
 end
