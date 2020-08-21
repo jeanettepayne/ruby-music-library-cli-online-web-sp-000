beefbf143b1c69c07cac2d1caab231ff1bb2a5e2
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-21 21:10:44 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -67,7 +67,8 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.chomp.to_i
-    input_toindex = input - 1
+    input_toindex = input 
+    if (1..)
     song = Song.all[input_toindex]
     puts "Playing #{song.name} by #{song.artist.name}" if song
   end
