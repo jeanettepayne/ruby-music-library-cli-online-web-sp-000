19b346bef0fec515e759c3cd5be7b9619407fc25
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-21 20:44:34 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -36,4 +36,9 @@ class MusicLibraryController
     end
   end
   
+  def list_genres
+    Genre.all.sort_by(&:name).each.with_index(1) do |genre, indx|
+      puts "#{indx}. #{genre.name}"
+  end
+  
 end
