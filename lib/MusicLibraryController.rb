03a6ddeb0181b2a98ffb805c4bdbdd4082432d1b
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-21 20:38:48 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -26,7 +26,7 @@ class MusicLibraryController
   
   def list_songs
     Song.all.sort {|a, b| a.name <=> b.name}.each_with_index(1) do |song, indx|
-      puts "#{indx}. #{song.artist.name} - #{song."
+      puts "#{indx}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
   end
   
 end
